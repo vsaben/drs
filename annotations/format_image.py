6c92c9ca64c9914337cabfc94464d4f610efaa9d
Vaughn <vsaben@outlook.com>
2020-02-12 15:15:09 +0200

    added original depth and stencil conversions
    conversions must be checked

--- annotations/format_image.py
+++ annotations/format_image.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-# A: Convert Tif to Jpeg
+# 1: Functions
 
-def TifToJpeg(image_path, resize_dims = None):
+def tif_to_jpeg(image_path, resize_dims = None):
     
     # Function: Converts a single Tif file to Jpeg and removes the corresponding Tif file
     # Output: New Jpeg file directory
@@ -15,7 +15,8 @@ def TifToJpeg(image_path, resize_dims = None):
     image_raw = Image.open(image_path)
     image_array = np.array(image_raw)
     image_rgb = image_array[:, :, 0:3]        
-    if resize: 
+    
+    if resize_dims is not None: 
         resized_image = Image.fromarray(image_rgb).resize(resize_dims)
         image_rgb = np.array(resized_image)
 
@@ -27,3 +28,26 @@ def TifToJpeg(image_path, resize_dims = None):
     os.remove(image_path)
 
     return image_path_jpeg
+
+def depth_to_usable_depth(depth_path):
+
+    # Function: Makes original depth image visible
+    # Check: Pixel linearisation
+
+    im_depth = Image.open(depth_path, mode = "r")                                             # Open
+    im_depth_pixel = list(im_depth.getdata())                                                 # Pixel information
+
+    im_depth_epixel = [math.exp(x) for x in im_depth_pixel]                                   # Linearised pixels [Unlikely correct]
+    im_depth_array = np.array(im_depth_epixel, dtype = "float32").reshape((720, 1280))        # Reshape array
+
+    plt.imsave(depth_path, im_depth_array)                                                    # Save plot (overwrite)
+
+def stencil_to_usable_stencil(stencil_path):
+
+    # Function: Makes original stencil image visible
+    # Check: Loss of per pixel flags 
+
+    im_stencil = Image.open(stencil_path)                                                     # Open    
+    im_stencil_array = np.array(im_stencil, dtype = "uint8").reshape((720, 1280))             # Reshape array
+
+    plt.imsave(stencilpath, im_stencil_array)          
