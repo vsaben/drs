84bd628a41ee9417fa084f70cb4b7414cdbbaf7f
Vaughn <vsaben@outlook.com>
2020-03-24 14:29:31 +0200

    removed unused reference

--- gta/Script.cs
+++ gta/Script.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 
 using GTA;
-using GTA.Native;
 using NativeUI;
 
 namespace DRS
